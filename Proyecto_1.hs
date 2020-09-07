36d55ea62ca676137dd583cce6826dd473d7893c
SantoII-github <SantoII1735@gmail.com>
2020-09-07 19:45:01 -0300

    Finished Proyecto_1.hs

--- Proyecto_1.hs
+++ Proyecto_1.hs
@@ -163,9 +163,244 @@ d) ¿Se te ocurre como redefinir factorial (ej. 2d) para evitar usar recursión?
 factorial' :: Int -> Int 
 factorial' x = productoria' [1..x] id
 
-{-ç
+{-
 e) multiplicaPares :: [Int] -> Int que calcula el producto de todos los números pares de una lista.
 -}
 
 multiplicaPares :: [Int] -> Int
 multiplicaPares xs = productoria' (filter even xs)  id
+
+{-
+7. Indagá en Hoogle sobre las funciones map y filter. También podes consultar su tipo en
+ghci con el comando :t
+
+-¿Qué hacen estas funciones?
+La función map f xs toma dos parámetros, una función f y una lista xs, y devuelve la lista de los resultados
+de aplicar f a cada elemento de xs.
+
+La función filter p xs toma dos parámetros, un predicado p y una lista xs, y devuelve la lista de los elementos
+de xs que cumplen con ese predicado. ("Filtra" la lista y la devuelve quitándole los elementos que no cumplen p).
+
+-¿A qué equivale la expresión map succ [1,-4, 6, 2,-8], donde succ n = n + 1?
+La expresión map succ [1,-4, 6, 2,-8] devuelve la lista de los números enteros que le siguen a cada elemento
+de [1,-4, 6, 2, -8]. Es decir, devolvería [2,-3, 7, 3,-7].
+
+-¿Y la expresión filter esPositivo [1,-4, 6, 2,-8]?
+La expresión filter esPositivo  [1,-4, 6, 2,-8] devuelve la lista de los números positivos de la lista
+[1, -4, 6, 2, -8]. Es decir, devolvería [1, 6, 2].
+
+8. Programá una función que dada una lista de números xs, devuelve la lista que resulta de duplicar cada 
+valor de xs.
+
+a) Definila usando recursión.
+-}
+
+duplicarLista :: [Int] -> [Int]
+duplicarLista [] = []
+duplicarLista (x:xs) = (2 * x) : duplicarLista xs  
+
+{-
+b) Definila utilizando la función map.
+-}
+
+duplicarLista' :: [Int] -> [Int]
+duplicarLista' xs = map (*2) xs
+
+{-
+9. Programá una función que dada una lista de números xs, calcula una lista que tiene como elementos
+aquellos números de xs que son pares.
+
+a) Definila usando recursión.
+-}
+
+soloPares :: [Int] -> [Int]
+soloPares [] = []
+soloPares (x:xs) 
+    | mod x 2 == 0 = x : soloPares xs
+    | otherwise = soloPares xs 
+
+{-
+b) Definila utilizando la función filter
+-}
+
+soloPares' :: [Int] -> [Int]
+soloPares' xs = filter even xs
+
+{-
+c) Revisá tu definición del ejercicio 6e. ¿Cómo podés mejorarla?
+
+Cuando resolví el ejercicio 6e ya conocía la existencia de la función filter. Lo que podría mejorar
+es usar la función definida en 9b para que leer la definición de la función multiplicaPares sea
+más fácil.
+-}
+
+multiplicaPares' :: [Int] -> Int
+multiplicaPares' xs = productoria' (soloPares' xs)  id
+
+{-
+10. La función primIgualesA toma un valor y una lista, y calcula el tramo inicial
+más largo de la lista cuyos elementos osn iguales a ese valor. Por ejemplo:
+
+primIgualesA 3 [3, 3, 4, 1] = [3, 3]
+primIgualesA 3 [4, 3, 3, 4, 1] = []
+primIgualesA 3 [] = []
+primIgualesA 'a' "aaadaa" = "aaa"
+
+a) Programá primIgualesA por recursión.
+-}
+
+primIgualesA :: Eq a => a -> [a] -> [a]
+primIgualesA _ [] = []
+primIgualesA y (x:xs)
+    | y == x = y : primIgualesA y xs
+    | otherwise = []
+
+{-
+Programá nuevamente la función utilizando takeWhile
+-}
+
+primIgualesA' :: Eq a => a -> [a] -> [a]
+primIgualesA' y xs = takeWhile (==y) xs
+
+{-
+11. La función primIguales toma una lista y devuelve el mayor tramo inicial de la
+lista cuyos elementos son todos iguales entre sí. Por ejemplo:
+
+primIguales [3,3,4,1] = [3,3]
+primIguales [4,3,3,4,1] = [4]
+primIguales [] = []
+primIguales "aaadaa" = "aaa"
+
+a) Programá primIguales por recursión.
+-}
+
+primIguales :: Eq a => [a] -> [a]
+primIguales [] = []
+primIguales [x] = [x]
+primIguales (x:xs)
+    | x == head xs = x : primIguales xs
+    | otherwise = [x]
+
+{-
+b) Usá cualquier versión de primIgualesA para programar pimIguales. Está permitido
+dividir en casos, pero no usar recursión. 
+-}
+
+primIguales' :: Eq a => [a] -> [a]
+primIguales' xs
+    | xs == [] = []
+    | otherwise = primIgualesA (head xs) xs
+
+{-
+12. (*) Para cada uno de los siguientes patrones, decidí si están bien tipados, y en tal caso da los
+tipos de cada subexpresión. En caso de estar bien tipado, ¿el patrón cubre todos los casos
+de definición?
+
+a) 
+f :: (a, b) -> ...
+f x = ...
+Está bien tipado. x :: (a,b). El patrón cubre todos los casos.
+
+b)
+f :: (a,b) -> ...
+f (x , y) = ...
+Está bien tipado. (x, y) :: (a,b). El patrón cubre todos los casos.
+
+c)
+f :: [(a,b)] -> ...
+f (a,b) = ...
+No está bien tipado porque el patrón pide una tupla cuando la función debería
+recibir una lista de tuplas.
+
+d)
+f :: [(a, b)] -> ...
+f (x:xs) = ...
+Está bien tipado. x :: (a,b), xs :: [(a,b)]. El patrón no cubre el caso de [].
+
+e)
+f :: [(a, b)] -> ...
+f ((x, y) : ((a, b) : xs)) = ...
+Está bien tipado. x :: a, y :: b, a :: a, b :: b, xs :: [(a,b)]. El patrón
+no cubre los casos de [] ni [x] 
+
+f)
+f :: [(Int, a)] -> ...
+f [(0, a)] = ...
+Está bien tipado. 0 :: Int, a :: a. El patrón no cubre los casos [], las listas con más de un elemento
+ni los casos en el que el primer elemento de la tupla no sea un 0.
+
+g)
+f :: [(Int, a)] -> ...
+f ((x, 1) : xs) = ...
+Está bien tipado. x :: Int, 1 :: Int, xs:: [(Int, Int)]. El patrón no cubre [] ni las listas en las que
+el segundo elemento de la tupla no es 1. 
+
+h)
+f :: [(Int, a)] -> ...
+f ((1, x) : xs) = ...
+Está bien tipado. 1 :: Int, x :: a, x :: [(Int, a)]. El patrón no cubre [] ni las listas en las que el
+primer elemento de sus tuplas no es 1.
+
+i)
+f :: (Int -> Int) -> Int -> ...
+f a b = ...
+Está bien tipado. a :: Int -> Int, b :: Int. El patrón cubre todos los casos.
+
+j)
+f :: (Int -> Int) -> Int -> ...
+f a 3 = ...
+Está bien tipado. a :: Int -> Int, 3 :: Int. El patrón no cubre los casos en los que el segundo parámetro
+no es 3. 
+
+k)
+f :: (Int -> Int) -> Int -> ...
+f 0 1 2 = ...
+Está mal tipada porque el patrón tiene 3 argumentos mientras que la función debería recibir 2. Además,
+ninguno de ellos es una función con tipo Int -> Int.
+
+l)
+f :: a -> (a -> a) -> ...
+f a g = ...
+Está bien tipada. a :: a, g :: a -> a. El patrón cubre todos los casos.
+
+13. (*) Para las siguientes declaraciones de funciones, da al menos una definición cuando sea
+posible (que no sea la expresión undefined). ¿Podés dar alguna otra definición alternativa
+a la que diste en cada caso?
+Por ejemplo, si la declaración es f :: (a, b) -> a, la respuesta es: f (x,y) = x
+
+a) f :: (a, b) -> b
+-}
+
+funcionA :: (a,b) -> b
+funcionA (_, b) = b
+
+{-
+b) f :: (a, b) -> c
+Sin especificar el tipo no se puede obtener un tipo que no estemos usando en los parámetros de la función
+
+c) f :: a -> b
+Sin especificar el tipo no se puede obtener un tipo que no estemos usando en los parámetros de la función
+
+d) f :: (a -> b) -> a -> b
+-}
+
+funcionD :: (a -> b) -> a -> b
+funcionD f a = f a 
+
+{-
+e) f :: (a -> b) -> [a] -> [b]
+-}
+
+funcionE :: (a -> b) -> [a] -> [b]
+funcionE _ [] = []
+funcionE f (x:_) = (f x) : []
+
+{-
+f ) f :: (a -> b) -> a -> c
+Sin especificar el tipo no se puede obtener un tipo que no estemos usando en los parámetros de la función
+
+g) f :: (a -> b) -> (b -> c) -> a -> c
+-}
+
+funcionF:: (a -> b) -> (b -> c) -> a -> c
+funcionF f1 f2 a = f2(f1(a))
